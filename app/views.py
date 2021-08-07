473e10337fb104b65bddfa1dfc0a7436a12a39f9
enockabere <maebaenock95@gmail>
2021-08-07 18:46:16 +0300

    create a title for my application

--- app/views.py
+++ app/views.py
@@ -8,8 +8,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = "The News Man"
     heading = "The News Man"
-    return render_template('index.html', heading = heading)
+    return render_template('index.html', heading = heading, title=title)
 @app.route('/articles/<int:articles_id>')
 def articles(articles_id):
     '''
