6541738eae228ab0f775e860981a49bdbb8077a4
enockabere <maebaenock95@gmail>
2021-08-07 18:07:47 +0300

    add a dynamic view for the articles page

--- app/views.py
+++ app/views.py
@@ -8,4 +8,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    heading = "The News Man"
+    return render_template('index.html', heading = heading)
+@app.route('/articles/<int:articles_id>')
+def articles(articles_id):
+    '''
+    it shows the news articles from the selected sources
+    '''
+    return render_template('articles.html',id=articles_id)
