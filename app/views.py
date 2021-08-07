0b42966f83c017ea05a5793d9fd56f492023b620
enockabere <maebaenock95@gmail>
2021-08-07 19:35:53 +0300

    add css to my index

--- app/views.py
+++ app/views.py
@@ -8,9 +8,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = "The News Man"
-    heading = "The News Man"
-    return render_template('index.html', heading = heading, title=title)
+    data = {
+        "title":"The News Man",
+        "heading": "The News Man" 
+    }
+    return render_template('index.html', context=data)
 @app.route('/articles/<int:articles_id>')
 def articles(articles_id):
     '''
