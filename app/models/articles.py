e4a3564938717d6008af6ea97393a15a31735304
enockabere <maebaenock95@gmail>
2021-08-08 02:03:56 +0300

    funtion to create article instances

---
+++ app/models/articles.py
@@ -0,0 +1,12 @@
+class Articles:
+    '''
+    class that defines articles objects
+    '''
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
