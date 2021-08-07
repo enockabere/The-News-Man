87b5d78956060b05ccdc8da35da67d2fc65ca5f0
enockabere <maebaenock95@gmail>
2021-08-08 02:15:08 +0300

    add function that getsjson response to artcles url request

--- app/request.py
+++ app/request.py
@@ -1,16 +1,20 @@
+from app.models.articles_test import Articles
+from app.views import articles
 from os import name
 from app import app
 import urllib.request,json
-from . models import source
+from . models import source,articles
 
 News_source = source.News_Source
+Articles = articles.Articles
 
 
 #Fetch API key
-api_key = app.config['MOVIE_API_KEY']
+api_key = app.config['NEWS_API_KEY']
 
 #Fetch News Base Url
 base_url = app.config["NEWS_API_BASE_URL"]
+article_url = app.config["ARTICLES_API_BASE_URL"]
 def get_sources():
     '''
     Function that gets the json response to our url request
@@ -48,4 +52,19 @@ def process_results(sources_list):
             sources_object = News_source(id,name,description,url,category,language,country)
             sources_result.append(sources_object)
     return sources_result
-    
+#Article Functions
+def get_articles(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = article_url.format(category,api_key)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+        
+        articles_results = None
+        if get_articles_response ['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = art_results (articles_results_list)
+    return articles_results
+        
