018dc1c289f781dc8b01764610c7deadbb6130b5
enockabere <maebaenock95@gmail>
2021-08-09 22:11:38 +0300

    a variable that takes in appliction instance

--- app/request.py
+++ app/request.py
@@ -1,17 +1,21 @@
-from app import app
 import urllib.request,json
-from . models import source,articles
+from .models import News_Source, Articles
 
-News_source = source.News_Source
-Articles = articles.Articles
+# News_source = source.News_Source
+# Articles = articles.Articles
 
 
 #Fetch API key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
 
 #Fetch News Base Url
-base_url = app.config["NEWS_API_BASE_URL"]
-article_url = app.config["ARTICLES_API_BASE_URL"]
+base_url = None
+article_url = None
+def configure_request(app):
+    global api_key,base_url,article_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    article_url = app.config['ARTICLES_API_BASE_URL']
 def get_sources():
     '''
     Function that gets the json response to our url request
@@ -46,7 +50,7 @@ def process_results(sources_list):
         language = source_item.get('language')
         country = source_item.get('country')
         if url:
-            sources_object = News_source(id,name,description,url,category,language,country)
+            sources_object = News_Source(id,name,description,url,category,language,country)
             sources_result.append(sources_object)
     return sources_result
 #Article Functions
