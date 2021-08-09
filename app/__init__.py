6a1682978216c30ed54c7c2c4a75020efcd31c8b
enockabere <maebaenock95@gmail>
2021-08-09 18:21:05 +0300

    register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from config import config_options
 
 def create_app(config_name):
     app = Flask(__name__)
+    
     #creating the app configuration
     app.config.from_object(config_options[config_name])
-    # will add views and forms
-    from app import views
-    from app import error
+    
+    #Registering the Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
     return app
