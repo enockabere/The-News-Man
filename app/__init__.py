8a12077bc57d0badf641b76ac29d5be80ca5313c
enockabere <maebaenock95@gmail>
2021-08-09 18:07:53 +0300

    create application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config=True)
-
-#setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-
-from app import views
-from app import error
+def create_app(config_name):
+    app = Flask(__name__)
+    #creating the app configuration
+    app.config.from_object(config_options[config_name])
+    # will add views and forms
+    from app import views
+    from app import error
+    return app
