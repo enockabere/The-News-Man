431180e0015e07fd236879078ba534f01d8b5462
enockabere <maebaenock95@gmail>
2021-08-14 00:11:48 +0300

    update sources

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,24 +1,8 @@
-$('#recipeCarousel').carousel({
-  interval: 10000
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-$('.carousel .carousel-item').each(function () {
-  var minPerSlide = 4;
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  for (var i = 0; i < minPerSlide; i++) {
-    next = next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
 $(document).ready(function () {
   $("#sCard").hover(function (event) {
     event.preventDefault();
