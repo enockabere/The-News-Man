13dd06ee4765e1298cd044be52cdc41c74b78913
enockabere <maebaenock95@gmail>
2021-08-13 19:57:00 +0300

    add scroll to top button

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -50,3 +50,22 @@ $ps.eq(3).after($('<button class="btn btn-success" id="mybut" type="button">Load
     $(this).remove();
     $ps.show();
 })); 
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
